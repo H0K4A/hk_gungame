9d0603c4532f1a4ac8466db160d18710bc1413f0
Hoka <128298619+H0K4A@users.noreply.github.com>
2025-10-20 16:23:28 +0200

    Correction et ajustement de la commande /gungame pour assurer l'affichage du menu dans cl_main.lua

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -1,87 +1,125 @@
--- Nettoyage et commentaires améliorés pour cl_main.lua
-
-local playerData = {
-  inGame = false,
-  instanceId = nil,
-  mapId = nil,
-  kills = 0,
-  weaponKills = 0,
-  currentWeapon = nil,
-  currentWeaponIndex = 0,
-  godMode = false,
-  playerName = nil,
-  lastSpawnPoint = nil
-}
-
-local hudVisible = true
-local killedEntities = {}
-local zoneBlip = nil
-local radiusBlip = nil
-local currentZoneData = nil
+-- Ajustements pour garantir le fonctionnement de la commande /gungame
 
 AddEventHandler('onClientResourceStart', function(resourceName)
-  if resourceName ~= GetCurrentResourceName() then return end
+    if resourceName ~= GetCurrentResourceName() then return end
 
-  print("^2[GunGame Client]^7 Script démarré")
+    print("^2[GunGame Client]^7 Script démarré")
 
-  if not lib then
-    print("^1[GunGame Client]^7 ERREUR: ox_lib n'est pas chargé!")
-    return
-  end
+    if not lib then
+        print("^1[GunGame Client]^7 ERREUR: ox_lib n'est pas chargé!")
+        return
+    end
 
-  print("^2[GunGame Client]^7 ox_lib détecté")
+    print("^2[GunGame Client]^7 ox_lib détecté")
 end)
 
 RegisterCommand('gungame', function()
-  TriggerEvent('gungame:openMenu')
+    TriggerEvent('gungame:openMenu')
 end, false)
 
-Citizen.CreateThread(function()
-  Wait(1000)
-  TriggerEvent('chat:addSuggestion', '/gungame', 'Ouvrir le menu GunGame', {})
-end)
-
-RegisterCommand('leavegame', function()
-  if playerData.inGame then
-    local ped = PlayerPedId()
-    local lastSpawn = playerData.lastSpawnPoint
-
-    playerData.inGame = false
-    playerData.instanceId = nil
-    playerData.mapId = nil
-    playerData.kills = 0
-    playerData.weaponKills = 0
-    playerData.currentWeapon = nil
-
-    RemoveAllPedWeapons(ped, true)
-    lib.hideTextUI()
-    removeGunGameZoneBlip()
-
-    if lastSpawn then
-      SetEntityCoords(ped, lastSpawn.x, lastSpawn.y, lastSpawn.z, false, false, false, false)
+RegisterNetEvent('gungame:openMenu')
+AddEventHandler('gungame:openMenu', function()
+    if not lib or not lib.callback then
+        lib.notify({
+            title = 'Erreur',
+            description = 'ox_lib n\'est pas chargé correctement',
+            type = 'error'
+        })
+        return
     end
 
-    Wait(300)
-    TriggerServerEvent('gungame:leaveGame')
+    local success, games = pcall(function()
+        return lib.callback.await('gungame:getAvailableGames', false)
+    end)
 
-    lib.notify({
-      title = 'GunGame',
-      description = 'Vous avez quitté la partie',
-      type = 'inform',
-      duration = 2000
-    })
-  else
-    lib.notify({
-      title = 'Erreur',
-      description = 'Vous n\'êtes pas en partie',
-      type = 'error'
-    })
-  end
-end, false)
+    local rotationInfo = nil
+    local rotationSuccess, rotationData = pcall(function()
+        return lib.callback.await('gungame:getRotationInfo', false)
+    end)
 
-Citizen.CreateThread(function()
-  Wait(1000)
-  TriggerEvent('chat:addSuggestion', '/leavegame', 'Quitter la partie GunGame', {})
+    if rotationSuccess and rotationData then
+        rotationInfo = rotationData
+    end
+
+    if not success or not games then
+        lib.notify({
+            title = 'Erreur',
+            description = 'Impossible de récupérer les parties disponibles',
+            type = 'error'
+        })
+        return
+    end
+
+    local options = {}
+
+    if rotationInfo then
+        local timeDisplay = string.format("%02d:%02d", rotationInfo.minutesUntil, rotationInfo.secondsUntil)
+
+        table.insert(options, {
+            title = '⏱️ PROCHAINE ROTATION',
+            description = rotationInfo.nextMapLabel .. ' dans ' .. timeDisplay,
+            icon = 'fa-solid fa-clock',
+            disabled = true
+        })
+
+        table.insert(options, {
+            title = '',
+            description = '',
+            icon = 'fa-solid fa-minus',
+            disabled = true
+        })
+    end
+
+    for _, game in ipairs(games) do
+        local isFull = game.currentPlayers >= game.maxPlayers
+        local isActive = game.isActive
+
+        local icon = "fa-solid fa-gamepad"
+        if isFull then
+            icon = "fa-solid fa-lock"
+        elseif isActive then
+            icon = "fa-solid fa-star"
+        end
+
+        local desc = ("Joueurs: %d/%d"):format(game.currentPlayers, game.maxPlayers)
+
+        if isActive then
+            desc = "🟢 ACTIVE | " .. desc
+        end
+
+        if isFull then
+            desc = desc .. ' [PLEIN]'
+        end
+
+        table.insert(options, {
+            title = game.label,
+            description = desc,
+            icon = icon,
+            disabled = isFull,
+            onSelect = function()
+                TriggerServerEvent('gungame:joinGame', game.mapId)
+            end
+        })
+    end
+
+    table.insert(options, {
+        title = '',
+        description = '',
+        icon = 'fa-solid fa-minus',
+        disabled = true
+    })
+
+    table.insert(options, {
+        title = 'Fermer le menu',
+        icon = 'fa-solid fa-xmark',
+        onSelect = function() end
+    })
+
+    lib.registerContext({
+        id = 'gungame_main_menu',
+        title = '🔫 GunGame - Sélectionnez une Map',
+        options = options
+    })
+
+    lib.showContext('gungame_main_menu')
 end)
-
--- Suite des améliorations et commentaires (inclus dans le code complet précédent)
